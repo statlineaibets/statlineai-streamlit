cfddf070396f6d51d1a6048c5e21c7938fbe3a97
statlineaibets <curtis.martinez32@yahoo.com>
2025-04-06 22:21:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-PRIZEPICKS_PROXY_URL = "https://prizepicks-proxy-bahamm34za-uc.a.run.app"
+PRIZEPICKS_PROXY_URL = "https://prizepicks-proxy-731823355083.us-central1.run.app"
 
 
 
